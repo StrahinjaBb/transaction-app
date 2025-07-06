15cdb3be1ec3f3d88e40834162b05f1006ad87df
Strahinja Bozovic <straleb123@gmail.com>
2025-07-06 18:44:04 +0200

    first commit - repo creation

---
+++ transaction-backend/src/main/java/com/example/transaction/TransactionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionBackendApplication.class, args);
+	}
+
+}
